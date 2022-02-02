f36e587a1ccd40b8e89f69f4f52f144ca8599d66
Mitchell Valdes-Bobes <miche.v.b@gmail.com>
2022-02-02 12:34:38 -0600

    data

--- 04 - 2022 Fall/Econ 810 Advanded Macroeconomic Theory/Part 1/PS 1/src/ps1.jl
+++ 04 - 2022 Fall/Econ 810 Advanded Macroeconomic Theory/Part 1/PS 1/src/ps1.jl
@@ -1,4 +1,4 @@
-using StatFiles, DataFrames
+using DataFrames, CSV
 
 df = DataFrame(load("04 - 2022 Fall/Econ 810 Advanded Macroeconomic Theory/Part 1/PS 1/data/pequiv_long.dta"))
 dropmissing!(df)
