d9fed5617b4c0effbde92f255e2d84ae0d1375cd
mitch <miche.v.b@gmail.com>
2022-02-10 23:37:18 -0600

    update

--- 04 - 2022 Fall/Econ 810 Advanded Macroeconomic Theory/Part 1/PS 2/src/PS2.jl
+++ 04 - 2022 Fall/Econ 810 Advanded Macroeconomic Theory/Part 1/PS 2/src/PS2.jl
@@ -2,9 +2,10 @@ using DataFrames, CSV, Statistics, Distributions, Plots, StatsPlots
 using StatsModels, GLM, FixedEffectModels, CategoricalArrays
 
 begin
-    theme(:vibrant); # :dark, :light, :plain, :grid, :tufte, :presentation, :none
-    default(fontfamily="Computer Modern", framestyle=:box); # LaTex-style
+    theme(:juno); # :dark, :light, :plain, :grid, :tufte, :presentation, :none
+    default(fontfamily="Computer Modern", framestyle=:box,); # LaTex-style
     gr(size = (800, 600)); # default plot size
+    Plots.scalefontsizes(1.75)
 end
 
 
@@ -16,46 +17,43 @@ PSID_data = CSV.read("04 - 2022 Fall/Econ 810 Advanded Macroeconomic Theory/Part
 μ_ΔIncome = mean(PSID_data.delta_income)
 
 ## Simulation
-N = 1000 # number of simulations
-T = 11 # Total years
-μ_Income = 30000 # mean of initial Income
-init_income = filter(row -> row.Year == 1979, PSID_data).income
-σ_ε = 1000 
-σ_ν = 1000
-dist_ε = Normal(0, σ_ε)
-dist_ν = Normal(0, σ_ν)
-dist_ζ = Normal(0, σ_ν)
+### Simulation parameters
+T = 11 # number of periods
+N_agents = 1000 # number of agents
+N_unemployed = 500 # number of unemployed agents
+N_employed = N_agents - N_unemployed # number of employed agents
+# idiosyncratic component
+σ_ε = 1000.0
+μ_ε = 0.0
+# time component
+γ_1 = 30000.0
+γ_t = 1000.0
+# Unemployment shock
+unemp_t = 5
+unemp_shock = -9000.0
 
-### Generate simulated sample
-initial_sample = μ_Income*ones(N) + rand(dist_ε, N)
+### Simulation
+ε = rand(Normal(μ_ε, σ_ε), N_agents, T)
+γ = γ_1 * ones(N_agents, T) + γ_t * repeat(0:10, 1, N_agents)'
 
-data = zeros(N, T) # Preallocate space for data
-data[:,1] = initial_sample # Initialize first column
+# Draw sample of unemployed agents
+unemployed = sample(1:N_agents, N_unemployed, replace = false)
+employed = setdiff(1:N_agents, unemployed)
+# Create dummy variables for employed and unemployed
+D = [zeros(N_agents, unemp_t) ones(N_agents, T - unemp_t)]
+D[employed, :] .= 0
 
-# Simulate 5 years of full employment
-for i ∈ 2:5
-    data[:,i] = data[:,i-1] + rand(dist_ν, N)
+data = γ + ε + unemp_shock * D
+
+begin
+    plot( data[unemployed, :]', color=1, legend=false , alpha=0.05)
+    plot!( data[employed, :]', color=2, legend=false, alpha=0.05) 
+    xticks!(1:11)
+
+    plot!( mean(data[unemployed, :], dims=1)', color=1, lw=3)
+    plot!(mean(data[employed, :], dims=1)', color=2, lw=3)
 end
 
-# Simulate a random unemployment shock 
-
-unemployed = sample(1:1000, 500, replace = false)
-# Simulate unemployed earnings shock
-data[unemployed,6] = data[unemployed,5] .- 9000 + rand(dist_ζ, Int(N/2))
-# Simulate employed earnings
-employed = [i for i ∈ 1:1000 if ~(i ∈ unemployed)]
-data[employed, 6]  = data[employed, 5] + rand(dist_ν, Int(N/2))
-
-# Simulate years 7 to 11
-for i ∈ 7:11
-    data[:,i] = data[:,i-1] + rand(dist_ν, N)
-end
-
-plot( data[unemployed, :]', color=1, legend=false , alpha=0.05)
-plot!( data[employed, :]', color=2, legend=false, alpha=0.05) 
-
-plot!( mean(data[unemployed, :], dims=1)', color=1, lw=3)
-plot!(mean(data[employed, :], dims=1)', color=2, lw=3)
 
 # Store data in DataFrame
 begin
@@ -65,21 +63,42 @@ begin
 
     
     df = DataFrame([:id => id, :year => year, :income => income])
-    years_from_layoff = df.year .- 6
+    years_from_layoff = df.year .- 5
     df.years_from_layoff = years_from_layoff
     df[employed, :years_from_layoff] .= Inf
     
+    df.years_from_layoff = [(y  < 6 ) ? y : missing for y in df.years_from_layoff]
     df[!, :years_from_layoff] = categorical(df[!, :years_from_layoff])
-
+    
+    # df.years_from_layoff = [(y != 6) ? y : missing for y in df.years_from_layoff]
 end
+
+levels(df.years_from_layoff)
 
 @df df[df.year .== 11, :] density(:income, legend = false, lw= 3)
 
-df.years_from_layoff = [(y != Inf) ? y : missing for y in df.years_from_layoff]
 
 # Estimate regression
-lag_ols_reg = lm(@formula(income ~ year + years_from_layoff), df)
+## Manual approach (not recommended I do not like it)
+## Create Dummy variables
+for i in 1:T
+    println(i-5)
+end
 
+## Using R
+using RCall
+reg = reval("lm(income ~ year + years_from_layoff) data = $df")
+
+lag_ols_reg = lm(@formula(income  ~  id + years_from_layoff), df,
+                        contrasts = Dict(:years_from_layoff => DummyCoding(),:id => EffectsCoding()))
+
+lag_ols_reg
+
+fit(df,  @formula( income ~ years_from_layoff + year))
+
+
+
+reg(df, @formula(income ~ fe(id) + fe(year) + years_from_layoff))
 
 
 CSV.write("04 - 2022 Fall/Econ 810 Advanded Macroeconomic Theory/Part 1/PS 2/data/income_diff_sim.csv", df)
